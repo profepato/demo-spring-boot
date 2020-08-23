c48c084e05d2e3dfa3706e6184728196dca727b7
pperezp <patodeath@gmail.com>
2020-08-22 23:34:30 -0400

    Add client Class to project

---
+++ src/main/java/cl/example/demo/domain/Client.java
@@ -0,0 +1,13 @@
+package cl.example.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client {
+    private int id;
+    private String name;
+}
